0342f1ec5e31f89b13749c93abc1c4d3c17bc817
Sean Brown <seanbrown82@hotmail.com>
2022-10-30 01:55:23 +0100

    Feat: Add login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import {Container, Form, Button} from 'react-bootstrap';
+import {Link} from "react-router-dom"
 
 export default function Login() {
   return (
-    <div>login</div>
+    <Container className="border border-1 border-secondary mt-5 py-5 rounded">
+        <h2 className="text-center">Log In</h2>
+        <Form className="w-75 mx-auto">
+            <Form.Group>
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control required type="email" placeholder="example@address.com"></Form.Control>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Password</Form.Label>
+                <Form.Control required type="password"></Form.Control>
+            </Form.Group>
+            <Button type="submit" className="w-100 mt-5">Log in</Button>
+        </Form>
+        <p className="text-center text-muted">Don't have an account? <Link to="/firebase-exercise/signup">Sign up</Link></p>
+        
+    </Container>
   )
 }
+
