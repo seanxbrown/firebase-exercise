2befb7296cc12903a5f9074b178ece80e4ad9062
Sean Brown <seanbrown82@hotmail.com>
2022-11-21 01:29:53 +0000

    Refactor: Update environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { getDatabase, set, ref, onValue, update } from "firebase/database";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDHvOIsm-BKBQ5u_1Tbx1qGOGzTbpjFbYI",
-  authDomain: "fir-exercise-85d6f.firebaseapp.com",
-  projectId: "fir-exercise-85d6f",
-  storageBucket: "fir-exercise-85d6f.appspot.com",
-  messagingSenderId: "431217489563",
-  appId: "1:431217489563:web:34d5b02db2a4c21c39b83e",
-  databaseURL: "https://fir-exercise-85d6f-default-rtdb.europe-west1.firebasedatabase.app/"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASEURL
 };
 
 // Initialize Firebase
