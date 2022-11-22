1b1c3d32145a9f5036e6783793b5544eab69b938
Sean Brown <seanbrown82@hotmail.com>
2022-11-22 22:23:03 +0000

    Update app name

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import { Row } from "react-bootstrap"
 const Footer = () => {
   return (
     <footer className="py-4 px-4 d-flex justify-content-between">
-        <p>&copy;Exercise Tracker App 2022</p>
+        <p>&copy;Metis 2022. Metis - the exercise tracker app.</p>
         <a href="https://github.com/seanxbrown/firebase-exercise" target="blank">Github repo</a>
         <a href="#header">To top</a>
     </footer>
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import {Link} from "react-router-dom"
 const Header = ( {logOut, user}) => {
   return (
     <header className="py-3 px-2 d-flex justify-content-between align-items-center text-light" id="header">
-        <Link to="/firebase-exercise" className="text-light h1 text-decoration-none fw-bold">Exercise Tracker App</Link>
+        <Link to="/firebase-exercise" className="text-light h1 text-decoration-none fw-bold">Metis</Link>
         <h5>{user && user.email}</h5>
         <a className="align-self-center text-decoration-none text-light" id="logout-text" onClick={() => {logOut()}}>Log Out</a>
   </header>  )
