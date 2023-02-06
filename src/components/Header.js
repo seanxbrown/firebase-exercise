a008f631df96fab537c644446a221b7d7981841c
Sean Brown <seanbrown82@hotmail.com>
2023-02-06 19:39:16 +0000

    Feat: Replace header with collapsable nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,31 @@
 import React from 'react'
 import {Link} from "react-router-dom"
+import { Navbar, Container, Nav } from "react-bootstrap"
 
 const Header = ({ logOut, user }) => {
   return (
-    <header className="py-3 px-2 d-flex justify-content-between align-items-center text-light" id="header">
-        <Link to="/firebase-exercise" className="text-light h1 text-decoration-none fw-bold">Metis</Link>
-        <h5>{user && user.displayName || user && user.email}</h5>
-        <Link to="/firebase-exercise/profile" className="text-light text-decoration-none">Profile</Link>
-        <a className="align-self-center text-decoration-none text-light" id="logout-text" onClick={() => {logOut()}}>Log Out</a>
-  </header>  )
+    <Navbar expand="lg" id="header">
+      <Container>
+        <Link to="/firebase-exercise" className="text-light navbar-brand fs-1 text-decoration-none fw-bold">Metis</Link>
+        <Navbar.Toggle/>
+        <Navbar.Collapse>
+
+          <Nav className="me-auto">
+            <Navbar.Text className="text-light">{user && user.displayName || user && user.email}</Navbar.Text>
+          </Nav>
+
+          <Nav>
+            <Link to="/firebase-exercise/profile" className="text-light text-decoration-none nav-link">Profile</Link>
+          </Nav>
+
+          <Nav>
+            <Nav.Link className="text-decoration-none text-light" id="logout-text" onClick={() => {logOut()}}>Log Out</Nav.Link>
+          </Nav>
+
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
 }
 
 export default Header
