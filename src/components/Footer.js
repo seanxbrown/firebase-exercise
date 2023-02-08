68df0eb8667fc145803de16099ff00bb97c79edd
Sean Brown <seanbrown82@hotmail.com>
2023-02-08 23:32:05 +0000

    Style: Change footer text colour

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="py-4 px-4 d-flex justify-content-between text-light">
-        <p>&copy;Metis 2022. Metis - the exercise tracker app.</p>
+    <footer className="py-4 px-4 d-flex justify-content-between">
+        <p className="text-light">&copy;Metis 2022. Metis - the exercise tracker app.</p>
         <a href="https://github.com/seanxbrown/firebase-exercise" target="blank">Github repo</a>
         <a href="#header">To top</a>
     </footer>
