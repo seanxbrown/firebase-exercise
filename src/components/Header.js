f4d0427aa5209c9a80aa06b1e1bf7c3f6774eda7
Sean Brown <seanbrown82@hotmail.com>
2023-02-11 17:43:15 +0000

    Style: Update homepage styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,7 +17,6 @@ const Header = ({ logOut, user }) => {
           <Nav>
             <Link to={`/firebase-exercise/profile`} className="text-light text-decoration-none nav-link">Profile</Link>
             <Link to={`/firebase-exercise/dashboard`} className="text-light text-decoration-none nav-link">Dashboard</Link> 
-
           </Nav>
 
           <Nav>
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,11 +3,13 @@ import { Link } from "react-router-dom"
 
 const Home = ({user}) => {
   return (
-    <div>
-        <h2>Home</h2>
-            <Link to="/firebase-exercise/signup">Sign Up</Link>
-            <Link to="/firebase-exercise/login">Log in</Link>
-
+    <div className="vh-100 d-flex flex-column justify-content-center">
+      <h2 className="text-center fw-bold ">The exercise tracker application</h2>
+      <div id="homeLinkContainer" className="d-flex justify-content-center align-items-center h-25">
+        <Link to="/firebase-exercise/signup" className="btn btn-lg homeLinkButton mx-1">Sign Up</Link>
+        <Link to="/firebase-exercise/login" className="btn btn-lg homeLinkButton mx-1">Log in</Link>
+      </div>
+      
     </div>
   )
 }
