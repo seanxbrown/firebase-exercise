ef4d26a54e01334f9a4a49d64f812f664bffb601
Sean Brown <seanbrown82@hotmail.com>
2023-02-15 23:07:48 +0000

    Style: Improve styling of deletion confirmation boxes

--- src/components/DeleteExercise.js
+++ src/components/DeleteExercise.js
@@ -4,9 +4,9 @@ import { Container, Button } from "react-bootstrap"
 const DeleteExercise = ({selectedExercise, closeExerciseDeletionBox, removeExerciseFromWorkout}) => {
 
   return (
-    <Container className="bg-danger text-dark d-flex justify-content-center align-items-center flex-column rounded p-2 w-50 position-absolute top-50 start-50 translate-middle" id="deleteExerciseBox">
-        <p>Delete this exercise?</p>
-        <div id="deleteConfirmationButtonContainer" className="d-flex justify-content-between w-25">
+    <Container className="bg-danger bg-gradient border border-1 border-white rounded p-4 w-50 position-absolute top-50 start-50 translate-middle" id="deleteExerciseBox">
+        <p className="text-dark text-center">Delete this exercise?</p>
+        <div id="deleteConfirmationButtonContainer" className="d-flex justify-content-around w-50 gap-4 m-auto">
             <Button type="button" variant="dark" onClick={() => removeExerciseFromWorkout(selectedExercise)} >Yes</Button>
             <Button type="button" variant="dark" onClick={closeExerciseDeletionBox}>No</Button>
         </div>
--- src/components/DeleteWorkout.js
+++ src/components/DeleteWorkout.js
@@ -3,9 +3,9 @@ import { Container, Button } from "react-bootstrap"
 
 const DeleteWorkout = ({workout, closeWorkoutDeletionBox, removeWorkoutFromList}) => {
   return (
-    <Container className="bg-danger text-dark d-flex justify-content-center align-items-center flex-column rounded p-2 position-absolute top-50 start-50 translate-middle w-50" id="deleteWorkoutBox">
-        <p>Delete "{workout.title}"" (created on {workout.date})?</p>
-        <div id="deleteConfirmationButtonContainer" className="d-flex justify-content-between w-25">
+    <Container className="bg-danger bg-gradient rounded border border-1 border-white p-4 position-absolute top-50 start-50 translate-middle w-50" id="deleteWorkoutBox">
+        <p className="text-dark text-center">Delete "{workout.title}"" (created on {workout.date})?</p>
+        <div id="deleteConfirmationButtonContainer" className="d-flex justify-content-around w-50 gap-4 m-auto">
             <Button type="button" variant="dark" onClick={() => removeWorkoutFromList(workout.id)}>Yes</Button>
             <Button type="button" variant="dark" onClick={closeWorkoutDeletionBox}>No</Button>
         </div>
