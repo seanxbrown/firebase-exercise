030ea7e7907b0b356c591965bb86b93a9afbf1e0
Sean Brown <seanbrown82@hotmail.com>
2023-02-23 20:53:26 +0000

    Style: change edit button to a badge

--- src/components/ExerciseComponent.js
+++ src/components/ExerciseComponent.js
@@ -1,10 +1,10 @@
-import { CloseButton, Row, Col } from "react-bootstrap";
+import { CloseButton, Row, Col, Badge } from "react-bootstrap";
 
 const ExerciseComponent = ({ preview, exercise, openExerciseDeletionBox, selectExercise, openEditBox }) => {
 
     return (
         <>
-            <Row xs={3}className="justify-content-center text-center workoutData" id={exercise.id} onClick={() => selectExercise(exercise)}>
+            <Row xs={3}className="justify-content-center text-center workoutData align-items-center pb-3" id={exercise.id} onClick={() => selectExercise(exercise)}>
                 <Col sm={2}>
                     <p>{exercise.name}</p>
                 </Col>
@@ -24,15 +24,13 @@ const ExerciseComponent = ({ preview, exercise, openExerciseDeletionBox, selectE
                     <p>Notes: {exercise.notes}</p>
                 </Col>
                 <Col sm={1}>        
-                    {!preview && <p onClick={openEditBox}>Edit</p>}
+                    {!preview && <Badge pill bg="info" onClick={openEditBox}>Edit</Badge>}
                 </Col>
                 <Col sm={1}>
                     <CloseButton variant="white" className="border border-1 border-light" onClick={() => openExerciseDeletionBox(exercise)} />
                 </Col>
             </Row>
         </>
- 
-
     )
 }
 
--- src/components/WorkoutComponent.js
+++ src/components/WorkoutComponent.js
@@ -1,5 +1,5 @@
 import { useContext } from 'react'
-import { CloseButton, Row, Col } from "react-bootstrap"
+import { CloseButton, Row, Col, Badge } from "react-bootstrap"
 import { Link } from "react-router-dom"
 import { AuthContext } from '../contexts/AuthContext'
 
@@ -19,16 +19,16 @@ const WorkoutComponent = ({workout, selectWorkout, openWorkoutDeletionBox, openE
 }
 
   return (
-    <Row className="text-center workoutData" id={workout.id} onClick={(() => selectWorkout([workout.id]))}>
+    <Row className="text-center workoutData align-items-center" id={workout.id} onClick={(() => selectWorkout([workout.id]))}>
       <Col xs={5}>{workout.title}</Col>
       <Col xs={3}>{formatDate(workout.date)}</Col>
       <Col xs={1}>
         <Link to={`/firebase-exercise/workouts/${workout.id}`}>View</Link>
       </Col>
       <Col xs={1}>
-        {!preview && <p onClick={openEditBox}>Edit</p>}
+        {!preview && <Badge pill bg="info" onClick={openEditBox}>Edit</Badge>}
       </Col>
-      <Col xs={2} className="gx-5">
+      <Col xs={1} className="gx-5">
         <CloseButton variant="white" onClick={() => openWorkoutDeletionBox(workout)}/>
       </Col>
     </Row>
