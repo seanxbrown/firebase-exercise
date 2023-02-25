6b1e3f59dc61ca9c655f8489a56c660a82461d8d
Sean Brown <seanbrown82@hotmail.com>
2023-02-25 14:43:08 +0000

    Style: match profile header styling to dashboard header styling

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -23,7 +23,7 @@ const Profile = () => {
 
   return (
     <>
-        <h2 className="fw-bold text-center py-5">Profile</h2>
+        <h2 className="fw-bold text-center py-3">Profile</h2>
         <div className="border border-3 border-secondary rounded p-5 mb-5 w-75 m-auto" id="profileDiv">
           <div className="d-flex align-items-center border-bottom border-1 border-secondary">
               {updating ? 
