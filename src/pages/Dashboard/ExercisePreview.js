e6cb13b162ce67894f6c568be11132fea2260120
Sean Brown <seanbrown82@hotmail.com>
2023-03-04 13:07:33 +0000

    Feat: remove ability to add workouts and exercises from dashboard

--- src/pages/Dashboard/ExercisePreview.js
+++ src/pages/Dashboard/ExercisePreview.js
@@ -7,7 +7,6 @@ const ExercisePreview = ( { selectedWorkout, selectExercise, openExerciseDeletio
     <Col xs={12} sm={7} id="exerciseDiv" className="p-0">
         <div className="d-flex flex-column" id="exerciseColumnHeader">
             <h2 className="text-center">Exercises {selectedWorkout ? `(${selectedWorkout.title})` : null}</h2>
-            <Button type="button" onClick={toggleNewExerciseStatus} className="btn btn-primary align-self-center mb-3 rounded-pill">Add New Exercise</Button>
         </div>
         <div className="workoutDataContainer overflow-hidden">
             {selectedWorkout && selectedWorkout.exercises && selectedWorkout.exercises.length > 0 ? selectedWorkout.exercises.map(exercise => <ExerciseComponent preview="true" selectExercise={selectExercise} openExerciseDeletionBox={openExerciseDeletionBox} removeExerciseFromWorkout={removeExerciseFromWorkout} key={exercise.id} exercise={exercise}/>) : <h3 className="fw-bold text-center">No exercise information found.</h3>}
--- src/pages/Dashboard/WorkoutsPreview.js
+++ src/pages/Dashboard/WorkoutsPreview.js
@@ -21,7 +21,6 @@ const WorkoutsPreview = ({ toggleNewWorkoutStatus, workouts, openWorkoutDeletion
     <Col xs={12} sm={5} id="workoutDiv" className="border-end border-1 border-light p-1">
         <div className="d-flex flex-column" id="workoutColumnHeader">
             <h2 className="text-center">Workouts</h2>
-            <Button type="button" onClick={toggleNewWorkoutStatus} className="btn btn-primary align-self-center mb-3 rounded-pill">Add New Workout</Button>
         </div>
         <div className="workoutDataContainer overflow-hidden mb-4">
             {workouts && workouts.length > 0 ?
