aa3ffe1a87379acccff1c8833ce26e2f0f3c87f3
Sean Brown <seanbrown82@hotmail.com>
2023-03-12 13:08:57 +0000

    Style: improve positioning of workout and exercise deletion modals

--- src/components/DeletionModal.js
+++ src/components/DeletionModal.js
@@ -1,14 +1,14 @@
-import { Container, Button } from "react-bootstrap";
+import { Container, Button, Alert } from "react-bootstrap";
 
 const DeletionModal = ({ type, item, closeModal, removalFunction }) => {
   return (
-    <Container className="bg-danger bg-gradient border-dark rounded p-4 w-50 shadow-lg position-absolute top-50 start-50 translate-middle" id="deleteExerciseBox">
+    <Alert variant="danger" className="p-4 w-50 shadow-lg" id="deleteExerciseBox">
         <p className="text-dark text-center">Delete this {type}?</p>
         <div id="deleteConfirmationButtonContainer" className="d-flex justify-content-around w-50 gap-4 m-auto">
             <Button type="button" variant="dark" onClick={() => removalFunction(item.id)} >Yes</Button>
             <Button type="button" variant="dark" onClick={closeModal}>No</Button>
         </div>
-    </Container>
+    </Alert>
   )
 }
 
