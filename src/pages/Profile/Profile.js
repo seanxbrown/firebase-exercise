708af184ab08c246a9f9d72ce90fd4f26ff125ce
Sean Brown <seanbrown82@hotmail.com>
2023-04-05 12:45:24 +0100

    Style: update styling on profile page

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,6 +1,6 @@
 import { useState, useContext } from "react";
 import { AuthContext } from '../../contexts/AuthContext';
-import {Button, Form} from "react-bootstrap";
+import { Button, Form, Card } from "react-bootstrap";
 import { auth, updateProfile, sendPasswordResetEmail } from "../../firebase";
 import { Link } from "react-router-dom"
 import AlertModal from "../../components/AlertModal";
@@ -51,20 +51,24 @@ const Profile = () => {
     <>
         {alert && <AlertModal type={alertType} text={alertMessage} closeModal={closeErrorModal} />}
         <h2 className="fw-bold text-center py-3">Profile</h2>
-        <div className="border border-3 border-secondary rounded p-5 mb-5 w-75 m-auto text-dark" id="profileDiv">
-          <div className="d-flex align-items-center border-bottom border-1 border-secondary">
+        <Card className="border-0 p-5 mb-5 w-75 m-auto text-light" id="profileDiv">
+          <Card.Body className="d-flex align-items-center border-bottom border-1 border-light">
               {updating ? 
               <Form.Control 
                 id="displayNameInput" 
-                type="text" defaultValue={user.displayName} /> 
+                type="text" defaultValue={user.displayName}
+                /> 
                 : <p id="userDisplayName"><span className="fw-bold">Display Name: </span>{user.displayName ? `${user.displayName}` 
-                : "No Display Name Available"}</p>}
-              {!updating && <Button type="button" className="btn btn-primary p-1 ms-4 " onClick={() => setUpdating(true)}>Edit</Button> }
+                : "No Display Name Available"}</p>
+                }
+              {!updating && <Button type="button" className="btn btn-primary px-4 ms-4" onClick={() => setUpdating(true)}>Edit</Button> }
               {updating && <Button type="button" className="btn" onClick={updateUserProfile}>Save</Button>}
-          </div>
-          <p id="userEmail"><span className="fw-bold">Email Address: </span>{user.email}</p>
-          <p className="d-inline" id="passwordChange" onClick={resetPassword}>Change password</p>
-        </div>
+          </Card.Body>
+          <Card.Body>
+            <p id="userEmail"><span className="fw-bold">Email Address: </span>{user.email}</p>
+            <Button className="d-inline btn" id="passwordChange" onClick={resetPassword}>Change password</Button>
+          </Card.Body>          
+        </Card>
         <Link to={`/firebase-exercise/dashboard`}>Return to dashboard</Link>
     </>
   )
