8e73f28eea0b592b23624f2315888194f99c7a93
Sean Brown <seanbrown82@hotmail.com>
2023-11-20 21:51:37 +0000

    feat: display either dashboard link or guest login link depending on auth status

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,15 @@
 import { Link, useNavigate } from "react-router-dom"
 import { signInAnonymously, auth } from "../../firebase"
 import AlertModal from "../../components/AlertModal"
-import { useState } from "react"
+import { useState, useContext } from "react"
 import { Container, Row, Col } from "react-bootstrap"
+import { AuthContext } from "../../contexts/AuthContext"
 
 const Home = () => {
   const navigate = useNavigate()
    const [error, setError] = useState(false)
    const [errorMessage, setErrorMessage] = useState("")
+   const user = useContext(AuthContext)
 
   async function signInAsGuest() {
 
@@ -42,8 +44,8 @@ const Home = () => {
           <Row className="align-items-center justify-content-center m-auto gy-5">
             <Col xs={12} md={6}><Link to="/firebase-exercise/signup" className="btn btn-lg homeLinkButton w-100">Sign up</Link></Col>
             <Col xs={12} md={6}><Link to="/firebase-exercise/login" className="btn btn-lg homeLinkButton w-100">Log in</Link></Col>
-            <Col xs={12} md={6} className="text-center"><a onClick={signInAsGuest} className="link-primary" id="guestSignInLink">Continue as guest</a></Col>
-            <Col xs={12} md={6} className="text-center"><Link to="/firebase-exercise/dashboard">Go to Dashboard</Link></Col>
+            { !user && <Col xs={12} md={6} className="text-center"><a onClick={signInAsGuest} className="link-primary" id="guestSignInLink">Continue as guest</a></Col> }
+            { user && <Col xs={12} md={6} className="text-center"><Link to="/firebase-exercise/dashboard">Go to Dashboard</Link></Col> }
           </Row>
        </Container>
       </div>
