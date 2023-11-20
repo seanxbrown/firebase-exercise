5620729395f716a4706a619256ee5889bf20062c
Sean Brown <seanbrown82@hotmail.com>
2023-11-20 22:30:26 +0000

    feat: close navbar after selecting menu option

--- src/components/layouts/Navigation.js
+++ src/components/layouts/Navigation.js
@@ -7,20 +7,20 @@ const Navigation = ({ logOut }) => {
 const user = useContext(AuthContext)
   
   return (
-    <Navbar expand="lg" id="header">
+    <Navbar collapseOnSelect expand="lg" id="header">
       <Container>
-        <Link to="/firebase-exercise" className="text-light navbar-brand fs-1 text-decoration-none fw-bold">Metis</Link>
+        <Nav.Link as={Link} to="/firebase-exercise" href="/firebase-exercise" className="text-light navbar-brand fs-1 text-decoration-none fw-bold">Metis</Nav.Link>
         <Nav className="me-auto">
             <Navbar.Text className="text-light">{user && user.displayName || user && user.email || user && "Guest User"}</Navbar.Text>
-          </Nav>
+        </Nav>
         <Navbar.Toggle/>
         <Navbar.Collapse>
           <Nav>
-            <Link to={`/firebase-exercise/profile`} className="text-light text-decoration-none nav-link">Profile</Link>
-            <Link to={`/firebase-exercise/dashboard`} className="text-light text-decoration-none nav-link">Dashboard</Link> 
+            <Nav.Link as={Link} to={`/firebase-exercise/profile`} href="/firebase-exercise/profile" className="text-light text-decoration-none nav-link">Profile</Nav.Link>
+            <Nav.Link as={Link} to={`/firebase-exercise/dashboard`} href="/firebase-exercise/dashboard" className="text-light text-decoration-none nav-link">Dashboard</Nav.Link> 
           </Nav>
           <Nav>
-            <Nav.Link className="text-decoration-none text-light" id="logout-text" onClick={() => { logOut() }}>Log Out</Nav.Link>
+            <Nav.Link as={Link} className="text-decoration-none text-light" id="logout-text" onClick={() => { logOut() }}>Log Out</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
