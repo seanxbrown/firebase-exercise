90512953d47577e2308ca5397ca2c5d5ee338098
Sean Brown <seanbrown82@hotmail.com>
2025-03-09 14:46:21 +0000

    fix: Netlify build/deployment issue

---
+++ src/components/SpinnerIcon.js
@@ -0,0 +1,10 @@
+import Spinner from 'react-bootstrap/Spinner';
+
+const SpinnerIcon = () => {
+  return (
+    <Spinner animation="border" role="status">
+    </Spinner>
+  )
+}
+
+export default SpinnerIcon
